b0f0a51ac49ed3a84c7a3183b14918d574a3ece3
Raissa <rhaii.azevedo@gmail.com>
2025-09-12 23:56:21 -0300

    falta o backend

--- IFES-6-PERIODO/PROJETO INTEGRADO IV-A/landing_page/bd.sql
+++ IFES-6-PERIODO/PROJETO INTEGRADO IV-A/landing_page/bd.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE landing_faesa;
-
 USE landing_faesa;
 
 CREATE TABLE contatos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL,
-    mensagem TEXT NOT NULL,
+    telefone VARCHAR(20),
+    curso_interesse VARCHAR(100),
+    nota_enem DECIMAL(5,2), -- Nota do ENEM com duas casas decimais
+    mensagem TEXT,
     data_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
---
+++ IFES-6-PERIODO/PROJETO INTEGRADO IV-A/landing_page/enem.js
@@ -0,0 +1,42 @@
+function calcularDesconto() {
+    const nota = parseFloat(document.getElementById("notaEnem").value);
+    const resultado = document.getElementById("resultado");
+    
+    if (isNaN(nota) || nota < 0 || nota > 1000) {
+        resultado.innerHTML = "⚠️ Insira uma nota válida entre 0 e 1000.";
+        resultado.style.color = "#ff4444";
+        return;
+    }
+
+    let desconto = 0;
+
+    if (nota >= 900) desconto = 100;
+    else if (nota >= 800) desconto = 75;
+    else if (nota >= 700) desconto = 50;
+    else if (nota >= 600) desconto = 30;
+    else desconto = 10;
+
+    resultado.style.color = "#00ffcc";
+    resultado.innerHTML = `🎉 Parabéns! Você conquistou <strong>${desconto}%</strong> de desconto na 1ª mensalidade.`;
+}
+
+// Imagem Carrosel
+const track = document.querySelector(".carousel-track");
+const slides = Array.from(track.children);
+let index = 0;
+
+function moveCarousel() {
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }
+  const slideWidth = slides[0].getBoundingClientRect().width;
+  track.style.transform = `translateX(-${index * slideWidth}px)`;
+}
+
+// troca de imagem a cada 3 segundos
+setInterval(moveCarousel, 3000);
+
+
+
+
